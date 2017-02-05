9233d85d95b2d383c54dd6df1346e8896431ec1f
Grace Stempel <gracestempel@Katara.home>
2017-02-04 22:06:32 -0500

    done

--- pic_god.c
+++ pic_god.c
@@ -15,12 +15,47 @@ int main() {
   write(fd, line, strlen(line) );
 
   for(i=0; i < 500; i++) {
-    for(j=0; j < 250; j++) {
+    for(j=0; j < 255; j++) {
 
-      r = j/2;
-      g = j/2;
-      b = 0;
+      if (j % 2 == 0 && j % 3 == 0) {
+	r = j/2;
+	g = 0;
+	b = j/2;
+      }
 
+      else if (j % 3 == 0) {
+	r = 0;
+	g = j/2;
+	b = j/2;
+      }
+
+      else {
+	r = j/2;
+	g = j/2;
+	b = 0;
+      }
+      snprintf(line, sizeof(line), "%d %d %d\n", r, g, b);
+      write(fd, line, strlen(line) );
+    }
+    for(j=255; j >= 0; j--) {
+
+      if (j % 2 == 0 && j % 3 == 0) {
+	r = j/2;
+	g = 0;
+	b = j/2;
+      }
+
+      else if (j % 3 == 0) {
+	r = 0;
+	g = j/2;
+	b = j/2;
+      }
+
+      else {
+	r = j/2;
+	g = j/2;
+	b = 0;
+      }
       snprintf(line, sizeof(line), "%d %d %d\n", r, g, b);
       write(fd, line, strlen(line) );
     }
